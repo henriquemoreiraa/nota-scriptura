76f9d37cef567fa20421201136d56d530cb44ab4
henriquemoreiraa <riqimoreiraa@gmail.com>
2024-02-03 22:50:27 -0300

    feat: add basic footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Nota Scriptura",
@@ -12,11 +13,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="flex justify-center h-screen">
-        <main className="w-full max-w-screen-xl p-3 flex justify-center items-center">
-          {children}
-        </main>
+    <html
+      lang="en"
+      className="w-full flex flex-col justify-center items-center h-screen"
+    >
+      <body className="flex flex-col max-w-screen-xl w-full h-full justify-center items-center">
+        <main className="flex-1 p-3">{children}</main>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <>
+      <div className="w-screen border-t border-zinc-200" />
+      <footer className="w-full p-3">
+        <div className="w-full max-w-screen-xl">
+          <p className="text-xs text-zinc-400">
+            Copyright &copy; 2024 Nota Scriptura.
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
